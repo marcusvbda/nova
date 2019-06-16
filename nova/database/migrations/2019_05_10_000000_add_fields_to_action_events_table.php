8a48f8348121ddc03d897d59e89f34073eb82451
Vinicius Bassalobre <=>
2019-06-16 14:45:53 -0300

    first commit

---
+++ nova/database/migrations/2019_05_10_000000_add_fields_to_action_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToActionEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('action_events', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->json('original')->nullable();
+            $table->json('changes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('action_events', function (Blueprint $table) {
+            $table->dropColumn('original', 'changes');
+        });
+    }
+}
