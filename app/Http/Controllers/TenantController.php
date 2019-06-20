5eb202f72aea4c5dace6a27f9bafe99086cddd15
Vinicius Bassalobre <=>
2019-06-20 16:14:12 -0300

    permit access to tenant select only who has more than 1 tenant

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -9,7 +9,20 @@ class TenantController  extends Controller
 {
     public function set()
     {
+        $this->canAccessSelection();
         return view("custom.tenants.set");
+    }
+    
+    private function canAccessSelection() 
+    {
+        $user = Auth::user();
+        if($user->tenants->count()>1)
+        {
+            $user->tenant_id = null;
+            $user->save();
+            return true;
+        }
+        abort(404);
     }
 
     public function get_options() 
