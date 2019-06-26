3e1c62afe074fb682a93020f36226fe41ab3e4dc
Vinicius Bassalobre <=>
2019-06-26 14:58:24 -0300

    add purple icon

--- nova/resources/views/auth/partials/header.blade.php
+++ nova/resources/views/auth/partials/header.blade.php
@@ -1,5 +1,5 @@
 <div class="mx-auto py-8 max-w-sm text-center text-90">
     {{-- @include('nova::auth.partials.logo') --}}
-    <img src="{{asset('imgs/logo.png')}}" style="width: 150px;"/>
+    <img src="{{asset('imgs/logo.svg')}}" style="width: 300px;"/>
     
 </div>
--- nova/resources/views/partials/logo.blade.php
+++ nova/resources/views/partials/logo.blade.php
@@ -1,3 +1,3 @@
 <div class="absolute pin-t pin-l pin-r bg-logo flex items-center w-sidebar h-header px-6 text-white" style="overflow: unset!important;">
-    <img src="{{asset('imgs/logo.png')}}" style="left: 38px;top: 2px;position: absolute; width: 150px;"/>
+    <img src="{{asset('imgs/logo.svg')}}"/>
 </div>
