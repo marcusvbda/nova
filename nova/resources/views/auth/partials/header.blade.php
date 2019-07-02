377fd5a681fe6e0602e97dac375f3e6f201261e5
Vinicius Bassalobre <=>
2019-07-02 11:06:54 -0300

    Change name to prisma

--- nova/resources/views/auth/partials/header.blade.php
+++ nova/resources/views/auth/partials/header.blade.php
@@ -1,5 +1,4 @@
 <div class="mx-auto py-8 max-w-sm text-center text-90">
     {{-- @include('nova::auth.partials.logo') --}}
-    <img src="{{asset('imgs/logo.svg')}}" style="width: 300px;"/>
-    
+    <img src="{{asset('imgs/logo-complete.png')}}" style="width: 300px;"/>
 </div>
